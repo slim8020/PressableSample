157c8059473b6ae0dad780d33986698239cd043e
slim8020 <slim8020@naver.com>
2021-06-29 17:51:36 +0900

    Add View Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  Text,
-  View,
-  Pressable,
-} from 'react-native';
+import { View } from 'react-native';
 import { Header, Contents, Footer } from './components/Layout';
 
-const Button = (props) =>{
-  return (
-    <Pressable
-    style={{padding: 10, backgroundColor:'#1abc9c'}}
-    onPressIn={()=>console.log('Press In')}
-    onPressOut={()=>console.log('Press Out')}
-    onPress={()=>console.log('Press')}
-    onLongPress={()=>console.log('Long Press')}
-    delayLongPress={3000}
-    hitSlop={10}>
-      <Text style={{}}>{props.title}</Text>
-    </Pressable>
-  );
-};
+
 
 const App = () => {
   return (
@@ -40,7 +15,6 @@ const App = () => {
         }}>
           <Header></Header>
           <Contents>
-             <Button title="Pressable"/>
           </Contents>
           <Footer></Footer>
          
